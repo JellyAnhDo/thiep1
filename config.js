b80a0f17b6fc03804291aab166084527a46daf0d
JellyAnhDo <dohoanganh444@gmail.com>
2023-10-20 18:12:27 +0700

    first version

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '20/10';
+const giftUrl = '';
+const eventName = `Happy women's day`;
+const titleCard = 'Gửi đến các ce';
+const contentCard = 'Chúc ce ngày 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với ce trong hôm nay và cả những ngày sau...';
+const giftImage = '';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
